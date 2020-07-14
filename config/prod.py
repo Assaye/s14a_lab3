590dbbc76dd2e0bc5fc3ec8c9a47f18e68e0e16a
Assaye Bulti <bulti.ta@gmail.com>
2020-07-14 04:39:11 +0100

    production

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,4 @@
 DEBUG = False
 SECRET_KEY = 's14a_key'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:HCMCTWAG@1@localhost:5432/book_catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
